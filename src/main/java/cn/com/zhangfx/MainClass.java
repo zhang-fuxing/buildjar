5f453e594a3dea3e5da6c22a9b190dfe579fe0f4
zhangfx <zhangfuxing1010@163.com>
2024-03-21 09:30:55 +0800

    Initial commit

---
+++ src/main/java/cn/com/zhangfx/MainClass.java
@@ -0,0 +1,13 @@
+package cn.com.zhangfx;
+
+/**
+ * @author 张福兴
+ * @version 1.0
+ * @date 2024/3/21
+ * @email zhangfuxing1010@163.com
+ */
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
